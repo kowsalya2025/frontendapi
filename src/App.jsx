41373be91a66b9b5f7f834c07150bc4d01bda7c6
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-17 01:07:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProjectList from "./components/ProjectList";
@@ -6,32 +6,35 @@ import ProjectList from "./components/ProjectList";
 // ProtectedRoute component
 function ProtectedRoute({ children }) {
   const isAuthenticated = !!localStorage.getItem("access_token"); // check if token exists
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
 }
 
 function App() {
   return (
-    <Routes>
-      {/* Protected home page */}
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <ProjectList />
-          </ProtectedRoute>
-        }
-      />
+    <Router>
+      <Routes>
+        {/* Protected home page */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <ProjectList />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Public routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-      {/* Redirect unknown routes to login */}
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+        {/* Redirect unknown routes to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
 
