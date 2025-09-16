eafd93e8023f7dc4bbd5f3c7a1af5ada7a42bc9b
kowsalya2025 <kowsalyacse1992@gmail.com>
2025-09-17 01:20:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProjectList from "./components/ProjectList";
-
-// ProtectedRoute component
-function ProtectedRoute({ children }) {
-  const isAuthenticated = !!localStorage.getItem("access_token"); // check if token exists
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-}
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Protected home page */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected route for ProjectList */}
         <Route
           path="/"
           element={
@@ -22,19 +20,13 @@ function App() {
             </ProtectedRoute>
           }
         />
-
-        {/* Public routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Redirect unknown routes to login */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
